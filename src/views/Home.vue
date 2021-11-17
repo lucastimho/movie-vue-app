15b4e824ebb1e091d0873ad045c39a776e424c28
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-16 21:39:38 -0600

    adds Vue CRUD actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    Title
+    <input type="text" v-model="newMovieParams.title" />
+    Year
+    <input type="integer" v-model="newMovieParams.year" />
+    Plot
+    <input type="text" v-model="newMovieParams.plot" />
+    <button v-on:click="createMovie()">Add Movie</button>
+    <div v-for="movie in movies" :key="movie.id">
+      {{ movie }}
+      <button v-on:click="showMovie(movie)">More Info</button>
+    </div>
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info:</h1>
+        <p>
+          Title:
+          <input type="text" v-model="currentMovie.title" />
+        </p>
+        <p>
+          Year:
+          <input type="text" v-model="currentMovie.year" />
+        </p>
+        <p>
+          Plot:
+          <input type="text" v-model="currentMovie.plot" />
+        </p>
+        <p>
+          English:
+          <input type="text" v-model="currentMovie.english" />
+        </p>
+        <button v-on:click="updateMovie(currentMovie)">Update Movie</button>
+        <button v-on:click="destroyMovie(currentMovie)">Delete</button>
+        <button>close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+      newMovieParams: {},
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/movies").then((response) => {
+      this.movies = response.data;
+    });
+  },
+  methods: {
+    createMovie: function () {
+      axios
+        .post("http://localhost:3000/movies", this.newMovieParams)
+        .then((response) => {
+          console.log(response.data);
+          this.products.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
+  showMovie: function (movie) {
+    console.log(movie);
+    this.currentMovie = movie;
+    document.querySelector("#movie-details").showModal();
+  },
+  updateMovie: function (movie) {
+    axios.patch("http://localhost:3000/movies/" + movie.id, movie).then((response) => {
+      console.log(response.data);
+    });
+  },
+  destroyMovie: function (movie) {
+    axios.delete("http://localhost:3000/movies/" + movie.id).then((response) => {
+      console.log(response.data);
+      var index = this.movies.indexOf(movie);
+      this.movies.splice(index, 1);
+    });
   },
 };
 </script>
